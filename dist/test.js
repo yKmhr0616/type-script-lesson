fd17a33ed382eb273a2af8438468093a4f57280e
yKmhr0616 <yu.kamahara0616@gmail.com>
2021-08-24 00:19:51 +0900

    ts序盤学習中&プッシュテスト

---
+++ dist/test.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.test2 = exports.test1 = void 0;
+function test1() {
+    return 'test1';
+}
+exports.test1 = test1;
+function test2() {
+    return { value: 'test2' };
+}
+exports.test2 = test2;
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+export function test1(){
+    return 'test1'
+}
+
+export function test2(){
+    return { value: 'test2'}
+}
